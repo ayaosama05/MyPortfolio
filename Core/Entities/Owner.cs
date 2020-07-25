36490f386c921cbb082dc9dd7c67956ff1ba87c4
Aya Osama <ayaosama0508@gmail.com>
2020-07-25 02:10:45 +0200

    Add project files.

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace Core
+{
+    public class Owner : EntityBase
+    {
+        public string FullName { get; set; }
+        public string Job { get; set; }
+        public string Avatar { get; set; }
+        public Address Address { get; set; }
+    }
+}
---
+++ Core/Entities/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace Core
+{
+    public class Project : EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
